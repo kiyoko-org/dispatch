83caddfac936d6498feae4636b029998c22b2516
Ziggs25 <phathoez123@gmail.com>
2025-10-16 11:06:34 +0800

    only build arm64

--- scripts/build-android-on-mac.sh
+++ scripts/build-android-on-mac.sh
@@ -3,4 +3,10 @@
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 
-bunx expo run:android
+# Build and run with only arm64-v8a to avoid Windows 260 character path limit
+cd android
+.\gradlew.bat installDebug -PreactNativeArchitectures=arm64-v8a
+cd ..
+
+# Start Metro bundler
+npx expo start
