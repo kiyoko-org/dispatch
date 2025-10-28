c377da16af0186f9a6865859dac31d09c20880a1
mrndstvndv <stvndvmrnd@gmail.com>
2025-10-28 19:57:15 +0800

    sort location dropdowns alphabetically

--- lib/locations.ts
+++ lib/locations.ts
@@ -16,18 +16,20 @@ export type Regions = {
 
 const locations: Regions = data;
 
+const compareStrings = (a: string, b: string) => a.localeCompare(b, undefined, { sensitivity: 'base' });
+
 export const getProvinces = (): string[] => {
   const provinces: string[] = [];
   for (const region in locations) {
     provinces.push(...Object.keys(locations[region]));
   }
-  return [...new Set(provinces)];
+  return [...new Set(provinces)].sort(compareStrings);
 };
 
 export const getMunicipalities = (province: string): string[] => {
   for (const region in locations) {
     if (locations[region][province]) {
-      return Object.keys(locations[region][province]);
+      return Object.keys(locations[region][province]).sort(compareStrings);
     }
   }
   return [];
@@ -36,7 +38,7 @@ export const getMunicipalities = (province: string): string[] => {
 export const getBarangays = (province: string, municipality: string): string[] => {
   for (const region in locations) {
     if (locations[region][province] && locations[region][province][municipality]) {
-      return locations[region][province][municipality];
+      return [...locations[region][province][municipality]].sort(compareStrings);
     }
   }
   return [];
